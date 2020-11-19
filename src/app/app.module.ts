4e0d27e3401ee59e24c1c78cc1751551f01b3383
OrmazaAlvarez <lenin.antonio.ormaza12@hotmail.com>
2020-11-19 18:09:31 -0500

    Anidando varios componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ColoresComponent } from './colores/colores.component';
+import { SaludoComponent } from './saludo/saludo.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ColoresComponent
+    ColoresComponent,
+    SaludoComponent
   ],
   imports: [
     BrowserModule,
